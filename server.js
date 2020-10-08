cb1ad0b25c5ba360aa87bccc2042d456790884f7
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2020-10-08 13:06:48 -0500

    establish routes and paths

---
+++ server.js
@@ -0,0 +1,20 @@
+// Depenencies
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use(express.static('public'));
+
+// Port
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
